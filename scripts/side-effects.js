c100db944c0b1862518f2e3ce91c678a1229c8c1
Shiro Wang <startraceebr@foxmail.com>
2025-08-27 16:57:41 +0800

    chore: remove scripts

--- scripts/side-effects.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const fs = require('fs-extra')
-
-;(async () => {
-  const pkgPath = path.join(__dirname, '../package.json')
-  const json = await fs.readJson(pkgPath)
-  json.sideEffects = false
-  await fs.writeJson(pkgPath, json, { spaces: 2 })
-})()
